85bc6dab35403438511c71ef907c12a489f9fcd6
Fedenko Maksym <feden2906@gmail.com>
2021-03-25 09:06:27 +0200

    fix import

--- controllers/admin.controllers.js
+++ controllers/admin.controllers.js
@@ -1,3 +1,35 @@
-module.exports = {
+const { instanceTransaction } = require('../dataBase').getInstance();
+const { statusCodes, statusMessages, emailActionsEnum } = require('../constants');
 
+// const { userService, mailService } = require('../services');
+const userService = require('../services/user.services')
+const mailService = require('../services/mail.services')
+
+const getAllUsers = () => {};
+
+const changeUserStatus = () => {};
+
+const deleteUser = async (req, res, next) => {
+  const transaction = await instanceTransaction();
+  try {
+    const { params: { userID }, query: { prefLang = 'en' } } = req;
+
+    const { name, email } = await userService.findUserById(userID) || {};
+
+    await userService.deleteUser(userID, transaction);
+
+    await mailService.sendMail(email, emailActionsEnum.DELETE_ACCOUNT, { name });
+
+    await transaction.commit();
+
+    res.json(statusMessages.USER_WAS_DELETED[prefLang]);
+  } catch (e) {
+    await transaction.rollback();
+    next(e);
+  }
+}
+module.exports = {
+  getAllUsers,
+  changeUserStatus,
+  deleteUser
 }
