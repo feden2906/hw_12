b8383113911f4e961eb4aaeb1f403d0469274463
Fedenko Maksym <feden2906@gmail.com>
2021-03-30 14:32:44 +0300

    create migration for add columns to authTable

--- dataBase/migrations/20210326123150-add-createAT.js
+++ dataBase/migrations/20210326123150-add-createAT.js
@@ -2,11 +2,13 @@ const { rolesEnum, tableNames } = require('../../constants')
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.addColumn(tableNames.USERS, 'createAt', { type: Sequelize.DataTypes.STRING })
+    await queryInterface.addColumn(tableNames.AUTH, 'createdAt', { type: Sequelize.DataTypes.STRING })
+    await queryInterface.addColumn(tableNames.AUTH, 'updatedAt', { type: Sequelize.DataTypes.STRING })
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.removeColumn(tableNames.USERS, 'createAt')
+    await queryInterface.removeColumn(tableNames.AUTH, 'createdAt')
+    await queryInterface.removeColumn(tableNames.AUTH, 'updatedAt')
 
   }
 };
