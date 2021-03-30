9fa492569b92edac4fb22d52b829bd72277f5304
Fedenko Maksym <feden2906@gmail.com>
2021-03-30 14:01:52 +0300

    update config

--- configs/configs.js
+++ configs/configs.js
@@ -1,11 +1,13 @@
 module.exports = {
-  MONGO_URL: process.env.MONGO_URL || 'mongodb://localhost:27017/users_cars',
   JWT_SECRET: process.env.JWT_SECRET || 'MY_SECRET',
   JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET || 'MY_REFRESH_SECRET',
+
+  SENTRY_DSN: process.env.SENTRY_DSN || 'default',
+  MONGO_URL: process.env.MONGO_URL || 'mongodb://localhost:27017/users_cars',
   PORT: process.env.PORT || 5000,
 
-  ROOT_EMAIL: process.env.ROOT_EMAIL || 'qqq@gmail.com',
-  ROOT_EMAIL_PASSWORD: process.env.ROOT_EMAIL_PASSWORD || 'qqq',
+  ROOT_EMAIL: process.env.ROOT_EMAIL || 'default@gmail.com',
+  ROOT_EMAIL_PASSWORD: process.env.ROOT_EMAIL_PASSWORD || 'default',
 
   DB_LOGIN: process.env.DB_LOGIN || 'root',
   DB_PASSWORD: process.env.DB_PASSWORD || 'root',
