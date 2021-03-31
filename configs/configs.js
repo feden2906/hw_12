2f193da9b8b3e471ad5dd096e78e1b3340a6dec1
Fedenko Maksym <feden2906@gmail.com>
2021-03-31 23:18:15 +0300

    qwe

--- configs/configs.js
+++ configs/configs.js
@@ -2,12 +2,12 @@ module.exports = {
   JWT_SECRET: process.env.JWT_SECRET || 'MY_SECRET',
   JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET || 'MY_REFRESH_SECRET',
 
-  SENTRY_DSN: process.env.SENTRY_DSN || 'https://c86e2d9d79004ee6b0d13676189b2575@o561011.ingest.sentry.io/5697310',
+  SENTRY_DSN: process.env.SENTRY_DSN || 'dddd',
   MONGO_URL: process.env.MONGO_URL || 'mongodb://localhost:27017/users_cars',
   PORT: process.env.PORT || 5000,
 
-  ROOT_EMAIL: process.env.ROOT_EMAIL || 'maksona2906@gmail.com',
-  ROOT_EMAIL_PASSWORD: process.env.ROOT_EMAIL_PASSWORD || 'ozater12345',
+  ROOT_EMAIL: process.env.ROOT_EMAIL || 'dddddd',
+  ROOT_EMAIL_PASSWORD: process.env.ROOT_EMAIL_PASSWORD || 'ddddddd',
 
   DB_LOGIN: process.env.DB_LOGIN || 'root',
   DB_PASSWORD: process.env.DB_PASSWORD || 'root',
