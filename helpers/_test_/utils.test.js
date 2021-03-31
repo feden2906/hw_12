bbf21e77de8be2845d7d21a46dec3ac57339af1b
Fedenko Maksym <feden2906@gmail.com>
2021-03-31 11:22:52 +0300

    add test to nameNormalizator

---
+++ helpers/_test_/utils.test.js
@@ -0,0 +1,24 @@
+const { _nameNormalizator } = require('../utils');
+
+const nameNormalizatorData = [
+  { input: 'Jon Doe', output: 'Jon Doe' },
+  { input: 'Jon%Doe', output: 'Jon Doe' },
+  { input: '{Jon Doe}', output: 'Jon Doe' },
+  { input: '[Jon] Doe', output: 'Jon Doe' },
+  { input: '(Jon Doe)', output: 'Jon Doe' },
+  { input: 'Jon +Doe', output: 'Jon Doe' },
+  { input: '  Jon-Doe  ', output: 'Jon Doe' },
+  { input: '......JON      DOE....', output: 'Jon Doe' },
+  { input: 'JonDoe@gmail.com', output: 'Jondoe Gmail Com' }, // not work correctly with email
+  { input: '+38095544523', output: '38095544523' } // not work correctly with phone
+];
+
+describe('Test utils.js', () => {
+  test('Should return normalized name', () => {
+    nameNormalizatorData.forEach((testObj) => {
+      const name = _nameNormalizator(testObj.input);
+
+      expect(name).toBe(testObj.output);
+    });
+  });
+});
