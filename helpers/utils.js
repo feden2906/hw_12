e4ce6d3d7a57287d38ef5a5696f134d754bc221c
Fedenko Maksym <feden2906@gmail.com>
2021-03-31 11:22:23 +0300

    add nameNormalizator

--- helpers/utils.js
+++ helpers/utils.js
@@ -52,6 +52,21 @@ const _basicQueryBuilder = (query) => {
   };
 };
 
+const _nameNormalizator = (name = '') => {
+  if (!name) {
+    return '';
+  }
+
+  name = name.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+  name = name.replace(/[,.!@#$%^&*()<>?:"':;}\-\[\]{=+]/g, ' '); // Jon#%Doe => Jon Doe
+  name = name.split(' ').filter((char) => !!char); // Jon    Doe => [Jon,Doe]
+  name = name.map((string) => string.toLowerCase());
+  name = name.map((string) => string.charAt(0).toUpperCase() + string.slice(1));
+  name = name.join(' ').trim(); // [Jon,Doe] => Jon Doe
+
+  return name;
+};
+
 const _saveTokensToBD = async (userID, transaction) => {
   const tokens = tokenizer();
 
@@ -66,5 +81,6 @@ module.exports = {
   _basicQueryBuilder,
   _filesDirBuilder,
   _filesListSaver,
+  _nameNormalizator,
   _saveTokensToBD
 };
