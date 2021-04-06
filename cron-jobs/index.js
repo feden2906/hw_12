9e04c10697b1cff0d14ad283d3c43ec1b153e7a3
Fedenko Maksym <feden2906@gmail.com>
2021-04-06 23:17:11 +0300

    cron-job don`t work

--- cron-jobs/index.js
+++ cron-jobs/index.js
@@ -1,13 +1,14 @@
 const cron = require('node-cron');
 
-const { countUsers } = require('./calculate-statistic')
+const { countUsers } = require('./calculate-statistic');
+
 module.exports = () => {
   try {
     cron.schedule('*/10 * * * * *', () => {
       // Promise.allSettled([countUsers()]).then(() => console.log(1))
       // countUsers();
-    })
+    });
   } catch (e) {
-    console.log(e)   // не можна викидати вище/ вже в app
+    console.log(e); // не можна викидати вище/ вже в app
   }
-}
+};
